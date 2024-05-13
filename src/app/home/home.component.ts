539619ad9251b3f5b1293d1658a4bf77dd291c03
Deniz Seyrekbasan <dseyrekbasan@papatia.co>
2024-05-13 14:46:41 +0300

    added rating module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
 import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
+
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
-        console.log(products.items);
+        this.products = products.items;
       });
   }
 }
