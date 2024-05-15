709453be3cd487436b08601399040287d15d3af1
Deniz Seyrekbasan <dseyrekbasan@papatia.co>
2024-05-15 15:12:53 +0300

    Validator added

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../types';
 import { RatingModule } from 'primeng/rating';
@@ -15,11 +21,14 @@ import { ButtonModule } from 'primeng/button';
     FormsModule,
     RatingModule,
     ButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
   @Input() display: boolean = false; //parent to child
   @Output() displayChange = new EventEmitter<boolean>();
   @Input() header!: string;
@@ -33,8 +42,37 @@ export class EditPopupComponent {
 
   @Output() confirm = new EventEmitter<Product>(); //child to parent
 
+  specialCharacterValidator(): ValidatorFn {
+    return (control) => {
+      const hasSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(
+        control.value
+      );
+
+      return hasSpecialCharacter ? { hasSpecialCharacter: true } : null;
+    };
+  }
+
+  productForm = this.formBuilder.group({
+    name: ['', [Validators.required, this.specialCharacterValidator()]],
+    image: [''],
+    price: ['', [Validators.required]],
+    rating: [0],
+  });
+
+  ngOnChanges() {
+    this.productForm.patchValue(this.product);
+  }
+
   onConfirm() {
-    this.confirm.emit(this.product);
+    const { name, image, price, rating } = this.productForm.value;
+
+    this.confirm.emit({
+      name: name || '',
+      image: image || '',
+      price: price || '',
+      rating: rating || 0,
+    });
+
     this.display = false;
     this.displayChange.emit(this.display);
   }
